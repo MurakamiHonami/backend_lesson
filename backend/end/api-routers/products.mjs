5e8fcba0241d045f25c0ccf01c746647fc574919
tama <mnv2525@gmail.com>
2026-06-18 10:27:34 +0900

    コメントを追記

--- backend/end/api-routers/products.mjs
+++ backend/end/api-routers/products.mjs
@@ -7,38 +7,44 @@ const products = [
 ];
 
 const router=express.Router();
-router.#TODO('/products', function (req, res) {
+router.get('/products', function (req, res) {
+  //データ全体を返す
   res.json(products);
 });
 
 router.get('/products/:id', function (req, res) {
-  const target = products[req.params.id]
-  res.json(target);
+  //指定されたIDの商品を返す
+  const targetProduct = products[req.params.id]
+  res.json(targetProduct);
 });
 
-router.#TODO('/products', function (req, res) {
+router.post('/products', function (req, res) {
+  //新しい商品を商品リストに追加する
   const newProduct=req.body;
   products.push(newProduct);
-  console.log(products);
+  //追加した商品を返す
   res.json(newProduct);
 });
 
-router.#TODO('/products/:id', function (req, res) {
+router.delete('/products/:id', function (req, res) {
+  //指定されたIDの商品を削除する
   const deleteId=req.params.id;
   products.splice(deleteId,1);
-  console.log(products);
+  //削除した商品を返す
   res.json({deleteId});
 });
-router.#TODO('/products/:id', function (req, res) {
+router.patch('/products/:id', function (req, res) {
+  //指定されたIDの商品情報を更新する
   const targetProduct = products[req.params.id];
+  //nameに入力データがあればnameを更新
   if(req.body.hasOwnProperty('name')) {
     targetProduct.name=req.body.name;
   }
+  //priceに入力データがあればpriceを更新
   if(req.body.hasOwnProperty('price')) {
     targetProduct.price=req.body.price;
   }
-  console.log(products)
-
+  //更新した結果を返す
   res.json(targetProduct);
 });
 
---
+++ backend/start/api-routers/products.mjs
@@ -0,0 +1,52 @@
+import express, { Router } from "express";
+
+const products = [
+  { name: "table", price: 1000 },
+  { name: "chair", price: 100 },
+  { name: "clock", price: 700 },
+];
+
+const router=express.Router();
+router.get('/products', function (req, res) {
+  //データ全体を返す
+  res.json(products);
+});
+
+router.#TODO('/products/:id', function (req, res) {
+  //指定されたIDの商品を返す
+  const targetProduct = products[req.params.id]
+  res.json(targetProduct);
+});
+
+router.#TODO('/products', function (req, res) {
+  //新しい商品を商品リストに追加する
+  const newProduct=req.body;
+  products.push(newProduct);
+  //追加した商品を返す
+  res.json(newProduct);
+});
+
+router.#TODO('/products/:id', function (req, res) {
+  //指定されたIDの商品を削除する
+  const deleteId=req.params.id;
+  products.splice(deleteId,1);
+  //削除した商品を返す
+  res.json({deleteId});
+});
+router.#TODO('/products/:id', function (req, res) {
+  //指定されたIDの商品情報を更新する
+  const targetProduct = products[req.params.id];
+  //nameに入力データがあればnameを更新
+  if(req.body.hasOwnProperty('name')) {
+    targetProduct.name=req.body.name;
+  }
+  //priceに入力データがあればpriceを更新
+  if(req.body.hasOwnProperty('price')) {
+    targetProduct.price=req.body.price;
+  }
+  //更新した結果を返す
+  res.json(targetProduct);
+});
+
+
+export default router
